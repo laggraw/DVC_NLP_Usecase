22b41c7ac0fa640def5d56735078d1d2e821f48d
Lalit Aggarwal <agg.lalit@gmail.com>
2021-11-05 12:30:03 +0530

    Requirement.txt and set-up.py updated

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "DVC_NLP_Usecase"
+AUTHOR_USER_NAME = "laggraw"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -13,12 +13,12 @@ LIST_OF_REQUIREMENTS = []
 setup(
     name=SRC_REPO,
     version="0.0.1",
-    author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    author="laggraw",
+    description="A small package for DVC-NLP",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    url=f"https://github.com/laggraw/DVC_NLP_Usecase",
+    author_email="agg.lalit@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
